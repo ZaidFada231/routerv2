5965ec815e4e55d55770c62f2be0b192134763a3
ZaidFada231 <zaidfada01@gmail.com>
2023-05-22 15:31:56 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import NavigationBar from "./NavigationBar";
 import TriviaApp from "./trivia/TriviaApp";
 import WeatherApp from "./weather/WeatherApp";
 import NewsApp from "./news/NewsApp";
+import Game from "./tictactoe/TicApp";
 function App() {
   return (
     <Router>
@@ -14,6 +15,7 @@ function App() {
         <Route path="/app1" element={<TriviaApp />} />
         <Route path="/app2" element={<WeatherApp />} />
         <Route path="/app3" element={<NewsApp />} />
+        <Route path="/app4" element={<Game />} />
       </Routes>
     </Router>
   );
